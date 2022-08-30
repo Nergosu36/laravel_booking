e96312b40527daa0e536b5590d32cbd8f903e70c
azbytniewski <azbytniewski@cxindex.com>
2022-08-30 20:42:34 +0200

    Added reservation to database

---
+++ app/Models/Reservation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Traits\Timestamp;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Reservation extends Model
+{
+    use HasFactory, SoftDeletes, Timestamp;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'start_date',
+        'end_date',
+        'number_of_guests',
+    ];
+
+        /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
+}
---
+++ database/migrations/2022_08_30_183330_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedInteger('number_of_guests');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
